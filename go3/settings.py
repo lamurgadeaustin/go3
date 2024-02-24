392fb792326bfcd6681a8ffd2bb78e0fee1db329
Jeffrey Hogan <jhogan@hashicorp.com>
2024-02-23 21:11:45 -0600

    set env vars and such as well

--- go3/settings.py
+++ go3/settings.py
@@ -192,7 +192,7 @@ DATABASES = {"default": env.db()}
 # If the flag as been set, configure to use proxy
 if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
     DATABASES["default"]["HOST"] = "127.0.0.1"
-    DATABASES["default"]["PORT"] = 5432
+    DATABASES["default"]["PORT"] = 5434
 
 # [END cloudrun_django_database_config]
 
